8ca26a3050fbebe0c8d135e65a54ed89f1f0a648
Pranshu Namdeo <156303738+namdpran8@users.noreply.github.com>
2024-08-26 17:09:55 +0530

    craeted signup.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import Login from './pages/login/login'
 import './App.css'
+import SignUp from './pages/signup/signup'
 
 function App() {
   return <div className='p-4 h-screen flex items-center justify-center' >
-    <Login />
+    <SignUp />
   </div>
 }
 
