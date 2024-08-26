a9ee2b2c6e31814eb5e45e40aafc83a76ee131ba
Pranshu Namdeo <156303738+namdpran8@users.noreply.github.com>
2024-08-27 02:37:47 +0530

    connected backend to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import Login from './pages/login/login';
-import './App.css'
-import SignUp from './pages/signup/signup';
-import Home from './pages/home/home';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  return <div className='p-4 h-screen flex items-center justify-center' >
-    <Home />
-  </div>
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
 	return (
@@ -26,9 +27,9 @@ const Login = () => {
 							className='w-full input input-bordered h-10'
 						/>
 					</div>
-					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link  to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2'>Login</button>
