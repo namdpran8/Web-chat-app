4a584b5eed4f41eccdfa01fbed4552eb3d74493f
Pranshu Namdeo <156303738+namdpran8@users.noreply.github.com>
2024-08-27 01:07:12 +0530

    added logout LogoutButton

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import Conversations from "./Conversations"
+import LogoutButton from "./LogoutButton"
 import SearchInput from "./searchInput"
 
 function Sidebar() {
   return (
-    <div>
+    <div className=" border-r border-slate-500 p-4 flex flex-col">
 
         <SearchInput />
         <Conversations />
+        <LogoutButton />
         <div className="divider px-3"></div>
     </div>
   )
