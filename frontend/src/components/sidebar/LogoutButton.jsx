65746e5cc56bc956eceec817858d880177ae8e0b
Pranshu Namdeo <156303738+namdpran8@users.noreply.github.com>
2024-08-27 20:57:48 +0530

    added logout functionality

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,19 +1,17 @@
 import { BiLogOut } from "react-icons/bi";
-//import useLogout from "../../hooks/useLogout";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
-//	const { loading, logout } = useLogout();
+	const { loading, logout } = useLogout();
 
 	return (
 		<div className='mt-auto'>
-             <BiLogOut className='w-6 h-6 text-white cursor-pointer'/>
-        </div>
+			{!loading ? (
+				<BiLogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
 	);
 };
 export default LogoutButton;
-
-//{!loading ? (
-//   
-//) : (
-//    <span className='loading loading-spinner'></span>
-//)}
